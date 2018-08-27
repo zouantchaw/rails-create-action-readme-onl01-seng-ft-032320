77e395af26a638a1e65d9da3c66ad9eacec2b219
maxwellbenton <maxwellbenton@gail.com>
2018-08-27 14:53:59 -0400

    updated readme, starter code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,15 @@
 class PostsController < ApplicationController
-	def index
-		@posts = Post.all
-	end
+  def index
+    @posts = Post.all
+  end
 
-	def show
-		@post = Post.find(params[:id])
-	end
+  def show
+    @post = Post.find(params[:id])
+  end
 
-	def new
-		@post = Post.new
-	end
+  def new
+    @post = Post.new
+  end
+
+  # add create method here
 end
